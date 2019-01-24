aa30dfd2da2d936606669d3adc691c32a90cbf78
eajon <546464955@qq.com>
2019-01-24 09:46:01 +0800

    * 优化代码

--- RxHttp/src/main/java/com/github/eajon/base/RxBusActivity.java
+++ RxHttp/src/main/java/com/github/eajon/base/RxBusActivity.java
@@ -5,23 +5,13 @@ import android.os.Bundle;
 import com.threshold.rxbus2.RxBus;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 
-
-public abstract class RxBusActivity extends RxAppCompatActivity  {
-
-
-
+public abstract class RxBusActivity extends RxAppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         RxBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
     }
 
     @Override
