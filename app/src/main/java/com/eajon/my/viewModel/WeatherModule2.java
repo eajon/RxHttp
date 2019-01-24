3997d1ef4bfdbb3033375b06536876bfafd3618a
eajon <546464955@qq.com>
2019-01-24 09:30:43 +0800

    * 增加参数注解

--- app/src/main/java/com/eajon/my/viewModel/WeatherModule2.java
+++ app/src/main/java/com/eajon/my/viewModel/WeatherModule2.java
@@ -3,12 +3,18 @@ package com.eajon.my.viewModel;
 import android.arch.lifecycle.ViewModel;
 
 import com.eajon.my.model.BaseResponse;
+import com.eajon.my.model.Weather;
 import com.github.eajon.RxHttp;
 import com.github.eajon.annotation.Name;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherModule2 extends ViewModel {
 
     public void getWeather() {
+        //测试@NAME注解 对象会转成jsonObject 列表转成jsonArray
+        //value 对应参数名 不填使用变量名 require 设置成false 将过滤
         TestGson testGson = new TestGson();
         testGson.setCity2("常熟");
         testGson.setTest("111");
@@ -18,16 +24,19 @@ public class WeatherModule2 extends ViewModel {
         BaseResponse baseResponse = new BaseResponse();
         baseResponse.setCode(1);
         baseResponse.setMessage("HAHAH");
-        testGson.setBaseResponse(baseResponse);
+        testGson.setBaseResponse(null);
+        List<BaseResponse> arrayList = new ArrayList<>();
+        arrayList.add(baseResponse);
+        testGson.setResponseList(arrayList);
 //        HashMap map = new HashMap();
 //        map.put("city", "常熟");
         new RxHttp.Builder()
                 .get()
                 .baseUrl("http://wthrcdn.etouch.cn/")
                 .apiUrl("weather_mini")
-                .addObjectParameter(testGson)
+                .addObjectParameter(testGson)//可以使用name注解 过滤不需要的参数
 //                .addParameter(map)
-//                .entity(Weather.class)
+                .entity(Weather.class)
                 .tag("weather")
                 .build()
                 .request();
@@ -51,6 +60,16 @@ public class WeatherModule2 extends ViewModel {
 
 
         private BaseResponse baseResponse;
+
+        private List<BaseResponse> responseList;
+
+        public List<BaseResponse> getResponseList() {
+            return responseList;
+        }
+
+        public void setResponseList(List<BaseResponse> responseList) {
+            this.responseList = responseList;
+        }
 
         public BaseResponse getBaseResponse() {
             return baseResponse;
