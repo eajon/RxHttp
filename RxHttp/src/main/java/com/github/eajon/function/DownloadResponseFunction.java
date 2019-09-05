63261d9f84c9b20759c5984fff4d7886f7114d92
wengyijiong <wengyijiong@zfzx.cn>
2019-09-05 14:16:52 +0800

    * 代码修改

--- RxHttp/src/main/java/com/github/eajon/function/DownloadResponseFunction.java
+++ RxHttp/src/main/java/com/github/eajon/function/DownloadResponseFunction.java
@@ -1,6 +1,7 @@
 package com.github.eajon.function;
 
 import com.github.eajon.body.DownloadResponseBody;
+import com.github.eajon.task.DownloadTask;
 import com.github.eajon.util.FileUtils;
 
 import java.lang.reflect.Field;
@@ -11,16 +12,16 @@ import okhttp3.ResponseBody;
 /**
  * @author eajon
  */
-public class DownloadResponseFunction<T> implements Function<ResponseBody, Object> {
+public class DownloadResponseFunction<T extends ResponseBody> implements Function<ResponseBody, DownloadTask> {
 
     @SuppressWarnings(value = "unchecked")
     @Override
-    public T apply(ResponseBody responseBody) throws Exception {
+    public DownloadTask apply(ResponseBody responseBody) throws Exception {
         Class c = responseBody.getClass();
         Field field = c.getDeclaredField("delegate");
         field.setAccessible(true);
         DownloadResponseBody downloadResponseBody = ( DownloadResponseBody ) field.get(responseBody);
         FileUtils.write2File(downloadResponseBody);
-        return ( T ) downloadResponseBody.getDownloadTask();
+        return downloadResponseBody.getDownloadTask();
     }
 }
